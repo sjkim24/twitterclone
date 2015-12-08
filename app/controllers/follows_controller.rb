5b60fc8c7e5bae18b4c2ad0aa68f5f5ec8952293
Sung Jin Kim <sjkim0421@gmail.com>
2015-12-08 12:42:44 -0500

    add index, create, destroy to follows_controller. add those routes for follows

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,15 +1,23 @@
 class FollowsController < ApplicationController
 
   def index
-
+    @follows = Follows.all
+    render :index
   end
 
   def create
+    @follow = Follow.new(follow_params)
 
+    if @follow.save
+
+    else
+      flash.now[:errors] = @follow.errors.full_messages
+    end
   end
 
   def destroy
-
+    @follow = Follow.find(params[:id])
+    @follow.destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
   resources :tweets, except: [:index, :show]
+  resources :follows, only: [:index, :create, :destroy]
 end
