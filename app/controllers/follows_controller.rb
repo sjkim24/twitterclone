4f080abc2c08d59c4af1c37d4e7eb799adef777f
Sung Jin Kim <sjkim0421@gmail.com>
2015-12-08 13:43:04 -0500

    follow button allows users to only follow other users so far

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,23 +1,29 @@
 class FollowsController < ApplicationController
 
   def index
-    @follows = Follows.all
+    @follows = Follow.all
     render :index
   end
 
   def create
-    @follow = Follow.new(follow_params)
-
+    @follow = Follow.create(follow_params)
     if @follow.save
 
     else
       flash.now[:errors] = @follow.errors.full_messages
     end
+    redirect_to user_url(@follow.follower_id)
   end
 
   def destroy
     @follow = Follow.find(params[:id])
     @follow.destroy
+  end
+
+  private
+
+  def follow_params
+    params.require(:follow).permit(:follower_id, :user_id)
   end
 
 end
