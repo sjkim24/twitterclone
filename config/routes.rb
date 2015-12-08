01f9704b3e0c03dd057ceeecf68a632c7f710ab1
Sung Jin Kim <sjkim0421@gmail.com>
2015-12-07 20:51:03 -0500

    remove extra s from sessionss#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'sessionss#new'
+  root to: 'sessions#new'
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
   resources :tweets, except: [:index, :show]
