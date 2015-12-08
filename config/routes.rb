d4c9d885799fea4fa4f1bb4a0bc1f08c0af328dd
Sung Jin Kim <sjkim0421@gmail.com>
2015-12-07 20:50:16 -0500

    change root url to seesions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'users#show'
+  root to: 'sessionss#new'
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
   resources :tweets, except: [:index, :show]
