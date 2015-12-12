ccf1ea77565888cb2922ffc8c8b09283e70d335b
Sung Jin Kim <sjkim0421@gmail.com>
2015-12-12 15:06:25 -0500

    hard reset and run rails g backbone:install

---
+++ app/assets/javascripts/twitter.js
@@ -0,0 +1,15 @@
+window.Twitter =
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function () {
+    new Twitter.Routers.Router ({
+      $rootEl: $('#main')
+    });
+    Backbone.history.start();
+  }
+
+$(document).ready(function() {
+  Twitter.initialize();
+});
